74f91fb1efe2be07602b880575739bca09a69d2c
DESKTOP-PV67OOQ\tobia <tobi4504@edu.easj.dk>
2021-09-23 18:52:18 +0200

    CommitingVersion1.0

---
+++ TCPServer/TCPServer/Program.cs
@@ -0,0 +1,21 @@
+﻿using CLass_Libary;
+using System;
+using System.Text.Json;
+
+namespace TCPServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FootBallPlayer syntaxFinder = new FootBallPlayer(2, "Boris", 30, 2);
+           // Console.WriteLine(syntaxFinder.ToString());
+            string Syntax = JsonSerializer.Serialize(syntaxFinder);
+            Console.WriteLine(Syntax);
+            Server worker = new Server();
+            worker.Start();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ TCPServer/TCPServer/Server.cs
@@ -0,0 +1,101 @@
+﻿using CLass_Libary;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace TCPServer
+{
+    public class Server
+    {
+        private const int PORT = 2121;
+        static List<FootBallPlayer> listOfPlayers = new List<FootBallPlayer>();
+        private bool done = false;
+
+        public Server()
+        {
+
+        }
+        public void Start()
+        {
+            TcpListener listner = new TcpListener(IPAddress.Any,PORT);
+            listner.Start();
+
+            while(true)
+            {
+                TcpClient socket = listner.AcceptTcpClient();
+                Task.Run(
+                    () =>
+                    {
+                        TcpClient tmpSocket = socket;
+                        DoClient(tmpSocket);
+                    }
+                    );
+            }
+        }
+
+        private void DoClient(TcpClient socket)
+        {
+            using (StreamReader sr = new StreamReader(socket.GetStream()))
+            using (StreamWriter sw = new StreamWriter(socket.GetStream()))
+            {
+                sw.AutoFlush = true;
+                
+                while(!done)
+                { 
+                String playerString = sr.ReadLine();
+                    //try
+                    //{
+
+                        if (playerString.ToLower() == "hentalle")
+                        {
+                            foreach (var p in listOfPlayers)
+                            {
+                                sw.WriteLine(p.ToString());
+                            }
+                        }
+                        else if (playerString.Contains("ID"))
+                        {
+                            sw.WriteLine("Enter the id of the player you wish to find");
+                            playerString = sr.ReadLine();
+                            listOfPlayers.Where(p => p.Id == Convert.ToInt32(playerString));
+                        }
+                        else if (playerString.ToLower() == "gem")
+                        {
+                            sw.WriteLine("Input your Player");
+                            playerString = sr.ReadLine();
+                            FootBallPlayer player = JsonSerializer.Deserialize<FootBallPlayer>(playerString);
+                            listOfPlayers.Add(player);
+                            //Console.WriteLine("Received car json string " + playerString);
+                            //Console.WriteLine("Received car : " + player);
+                        }
+                        else if (playerString.ToLower() == "im done")
+                        {
+                            done = true;
+                        }
+                        else
+                        {
+                            sw.WriteLine("Your input was invalid");
+                        }
+                    //}
+                    //catch (NullReferenceException e)
+                    //{
+                    //    sw.WriteLine("you have to insert something");
+                    //}
+                    //catch(Exception e)
+                    //{
+                    //    sw.WriteLine(e.Message + "Pls contact your sever provider");
+                    //}
+                }
+
+
+            }
+            socket?.Close();
+        }
+
+    }
+}
